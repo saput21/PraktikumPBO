771e50605b93af492733fa5970bea3bea10a63ea
saput21 <100414151+saput21@users.noreply.github.com>
2023-03-08 17:45:11 +0700

    Add files via upload

---
+++ MOperasiTitik.java
@@ -0,0 +1,26 @@
+ //File		: MOperasiTitik.java	
+ //Penulis	: Thirafi Syahir Saputra
+ //NIM		: 24060121140139
+ //Deskripsi: Kelas yang berisi program utama yang memanfaatkan kelas operasi titik
+ 
+
+class MOperasiTitik{
+	public static void main(String[] args){
+		Titik t1 = new Titik(4.0, 4.0);
+		OperasiTitik op= new OperasiTitik();
+		
+		
+		System.out.println("Titik("+t1.getAbsis()+","+t1.getOrdinat()+")");
+		
+		
+		op.refleksiX(t1);
+		System.out.println("\nTitik setelah refleksi sumbu X");
+		System.out.println("Titik("+t1.getAbsis()+","+t1.getOrdinat()+")");
+		
+		
+		op.refleksiY(t1);
+		System.out.println("\nTitik setelah refleksi sumbu Y");
+		System.out.println("Titik("+t1.getAbsis()+","+t1.getOrdinat()+")");
+		
+	}
+}
