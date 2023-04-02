61b00ac40d9088876273532db223e25e8044ad80
saput21 <100414151+saput21@users.noreply.github.com>
2023-04-03 00:03:21 +0700

    Create IArea.java

---
+++ pertemuan5/Kelas Interface/IArea.java
@@ -0,0 +1,12 @@
+/** 
+* File      : IArea.java
+* Penulis   : Thirafi Syahir Saputra
+* Deskripsi : Interface yang mendefinisikan kontrak bahwa setiap kelas yang
+*			  membuat implementasi dari interface ini harus memiliki cara untuk
+*			  menghitung luas
+*
+*/
+
+public interface IArea{
+    public double hitungLuas();
+}
