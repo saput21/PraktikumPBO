01eab94daf6792d3ef4e6a994a5558fdfcda2ab5
saput21 <100414151+saput21@users.noreply.github.com>
2023-05-10 11:56:20 +0700

    Create Payroll.java

---
+++ Pertemuan 7/Tugas/Payroll.java
@@ -0,0 +1,12 @@
+/** 
+* File      : Payroll.java
+* Penulis   : Thirafi Syahir Saputra
+* Deskripsi : Kelas yang berupa metode untuk mencetak gaji pegawai
+*
+*/
+
+public class Payroll {
+    public void cetakGaji(Pegawai p){
+        p.tampilData();
+    }
+}
