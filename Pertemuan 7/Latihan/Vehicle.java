2b627b8771467435319a601ff16ae481cca15218
saput21 <100414151+saput21@users.noreply.github.com>
2023-05-10 11:52:03 +0700

    Update Vehicle.java

--- Pertemuan 7/Latihan/Vehicle.java
+++ Pertemuan 7/Latihan/Vehicle.java
@@ -1,3 +1,10 @@
+/** 
+* File      : Vehicle.java
+* Penulis   : Thirafi Syahir Saputra
+*
+*/
+
+
 public class Vehicle{
     void calRent (int distance, float price) {
         float fare=distance* price;
