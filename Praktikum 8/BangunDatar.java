685ac78d9f3bf22a2a80f87b9ecd6df41b7e85b5
saput21 <100414151+saput21@users.noreply.github.com>
2023-05-30 20:50:49 +0700

    Create BangunDatar.java

---
+++ Praktikum 8/BangunDatar.java
@@ -0,0 +1,11 @@
+/**
+ * File         : BangunDatar.java
+ * Penulis      : Thirafi Syahir Saputra
+ * NIM          : 24060121140139
+ * Deskripsi    : kelas abstrak untuk bangun datar
+ */
+
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+    
+}
