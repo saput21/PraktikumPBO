4b7b07ab7abea6c3b74610ca57c8a59f984d905d
saput21 <100414151+saput21@users.noreply.github.com>
2023-06-06 20:54:10 +0700

    Create PersonDAO.java

---
+++ Praktikum 9/PersonDAO.java
@@ -0,0 +1,11 @@
+/**
+ * File      : PersonDAO.java
+ * Penulis   : Thirafi Syahir Saputra
+ * NIM       : 24060121130062
+ * Deskripsi : interface untuk person access object
+ *
+ */
+
+public interface PersonDAO {
+    public void savePerson(Person p) throws Exception;
+}
